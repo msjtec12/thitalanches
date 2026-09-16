bd6be8e684660b96f510940522763c87e0ba0554
msjtec12 <msjtec12@gmail.com>
2026-09-16 16:05:17 -0300

    test: use Vitest-specific jest-dom integration

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,6 @@
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom/vitest';
 
-Object.defineProperty(window, "matchMedia", {
+Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: (query: string) => ({
     matches: false,
